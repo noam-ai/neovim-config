2acc3a1e65b47315a643f2d696e5ab7c32b74365
Noam Golani <noam@shape-ai.com>
2024-11-18 09:52:10 +0200

    Some changes

--- init.lua
+++ init.lua
@@ -107,6 +107,9 @@ vim.keymap.set('v', '<', '<gv', { desc = 'Shift text to the left' })
 vim.keymap.set('x', 'J', ":move '>+1<CR>gv-gv", { desc = 'Move selected lines down' })
 vim.keymap.set('x', 'K', ":move '<-2<CR>gv-gv", { desc = 'Move selected lines up' })
 
+-- Filetypes
+vim.filetype.add { extension = { templ = 'templ' } }
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -221,6 +224,7 @@ require('lazy').setup {
       vim.g.copilot_no_tab_map = true
     end,
   },
+
   {
     'dense-analysis/ale',
     config = function()
@@ -229,47 +233,61 @@ require('lazy').setup {
 
       -- Configure ALE
       g.ale_linters = {
-        python = { 'mypy', 'pylint' },
+        python = { 'ruff', 'pyright' },
+        sql = { 'sqlfluff' },
+        go = { 'golsp' },
       }
 
-      -- Python Config
+      -- Create a new sql fixer
+      local function sqlfluff_fix()
+        return {
+          command = 'sqlfluff fix --force --config ~/shape/monorepo/configs/.sqlfluff -',
+          formatStdin = true,
+        }
+      end
+
       g.ale_fixers = {
-        python = { 'black' },
+        python = { 'ruff' },
+        sql = { sqlfluff_fix },
+        json = { 'jq' },
+        go = { 'gofmt', 'goimports' },
       }
+
+      g.ale_sql_sqlfluff_options = '--dialect postgres --config ~/shape/monorepo/configs/.sqlfluff'
 
       g.ale_fix_on_save = 1
 
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-        pattern = { '*.py' },
-        callback = function(event)
-          -- Dfine a var
-          local file = event.file
-          -- If the file is in the monorepo
-          if string.find(file, 'shape/monorepo') then
-            -- Extract the project root, the folder after shape/monorepo
-            local index
-            if string.find(file, 'shape/monorepo%-wt1') then
-              index = string.find(file, 'shape/monorepo%-wt1') + string.len 'shape/monorepo-wt1'
-            else
-              index = string.find(file, 'shape/monorepo') + string.len 'shape/monorepo'
-            end
+      -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+      --   pattern = { '*.py' },
+      --   callback = function(event)
+      --     -- Dfine a var
+      --     local file = event.file
+      --     -- If the file is in the monorepo
+      --     if string.find(file, 'shape/monorepo') then
+      --       -- Extract the project root, the folder after shape/monorepo
+      --       local index
+      --       if string.find(file, 'shape/monorepo%-wt1') then
+      --         index = string.find(file, 'shape/monorepo%-wt1') + string.len 'shape/monorepo-wt1'
+      --       else
+      --         index = string.find(file, 'shape/monorepo') + string.len 'shape/monorepo'
+      --       end
 
-            if index == nil then
-              return
-            end
+      --       if index == nil then
+      --         return
+      --       end
 
-            local without_monorepo_root = string.sub(file, index + 1)
-            local slash_index = string.find(without_monorepo_root, '/')
-            local project_name = string.sub(without_monorepo_root, 0, slash_index - 1)
+      --       local without_monorepo_root = string.sub(file, index + 1)
+      --       local slash_index = string.find(without_monorepo_root, '/')
+      --       local project_name = string.sub(without_monorepo_root, 0, slash_index - 1)
 
-            local project_root = '~/shape/monorepo/' .. project_name .. '/'
-            -- Set the project root
-            g.ale_python_mypy_options = '--config-file ~/shape/monorepo/configs/mypy.ini'
-            g.ale_python_black_options = '--config ~/shape/monorepo/configs/pyproject.toml'
-            g.ale_python_pylint_options = "--rcfile '" .. project_root .. "pyproject.toml'"
-          end
-        end,
-      })
+      --       local project_root = '~/shape/monorepo/' .. project_name .. '/'
+      --       -- Set the project root
+      --       g.ale_python_mypy_options = '--config-file ~/shape/monorepo/configs/mypy.ini'
+      --       g.ale_python_black_options = '--config ~/shape/monorepo/configs/pyproject.toml'
+      --       g.ale_python_pylint_options = "--rcfile '" .. project_root .. "pyproject.toml'"
+      --     end
+      --   end,
+      -- })
     end,
   },
 
@@ -437,97 +455,56 @@ require('lazy').setup {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
-          -- NOTE: Remember that lua is a real programming language, and as such it is possible
-          -- to define small helper and utility functions so you don't have to repeat yourself
-          -- many times.
-          --
-          -- In this case, we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
           local map = function(keys, func, desc)
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-T>.
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-          -- Find references for the word under your cursor.
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
           map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-          -- Fuzzy find all the symbols in your current workspace
-          --  Similar to document symbols, except searches over your whole project.
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-          -- Rename the variable under your cursor
-          --  Most Language Servers support renaming across files, etc.
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-          -- Execute a code action, usually your cursor needs to be on top of an error
-          -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-          -- Opens a popup that displays documentation about the word under your cursor
-          --  See `:help K` for why this keymap
           map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-          -- WARN: This is not Goto Definition, this is Goto Declaration.
-          --  For example, in C this would take you to the header
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          --    See `:help CursorHold` for information about when this is executed
-          --
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
-          end
         end,
       })
 
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP Specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- Check docs for configurations here
         -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
         pyright = {},
+
+        templ = {
+          filetypes = { 'templ' },
+        },
+
+        html = {
+          capabilities = capabilities,
+          filetypes = { 'html', 'templ' },
+        },
+
+        htmx = {
+          capabilities = capabilities,
+          filetypes = { 'html', 'templ' },
+        },
+
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+                shadow = true,
+              },
+              staticcheck = true,
+            },
+          },
+        },
+
         lua_ls = {
           settings = {
             Lua = {
@@ -592,13 +569,20 @@ require('lazy').setup {
         timeout_ms = 500,
         lsp_fallback = false,
       },
+      formatters = {
+        sqlfmt = {
+          command = 'sqlfmt',
+          args = { '-' },
+        },
+      },
       formatters_by_ft = {
         lua = { 'stylua' },
+        templ = { 'templ' },
         -- Conform can also run multiple formatters sequentially
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
-        sql = { 'sqlfmt' },
+        go = { 'gofmt', 'goimports' },
       },
     },
   },
@@ -767,20 +751,17 @@ require('lazy').setup {
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'python', 'typescript' },
+        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'python', 'typescript', 'go', 'sql' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
       }
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
+
+    dependencies = {
+      'vrischmann/tree-sitter-templ',
+    },
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
